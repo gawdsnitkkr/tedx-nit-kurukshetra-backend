4d5bffb868ccb8be5445f4e88a896d555e47b883
nkkumawat <nk0kumawat@gmail.com>
2017-08-18 22:50:14 +0530

    Added delete speaker and video features

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
-var Storage = multer.diskStorage({
+var storage = multer.diskStorage({
     destination: function(req, file, callback) {
         callback(null, "./www/images");
     },
@@ -38,44 +38,61 @@ var Storage = multer.diskStorage({
     }
 });
 var upload = multer({
-    storage: Storage
+    storage: storage
 }).array("images", 3);
 app.get('/login' , function (req , res) {
-    if(req.session.user) {
+    if(req.session.username) {
         res.redirect('/admin');
     }else {
         res.sendFile(__dirname + '/www/login.html');
     }
 });
 app.post('/login' , function (req , res) {
-    login(req , res);
+    const post = req.body;
+    const username  = sanitizeHtml(post.user);
+    const password = sanitizeHtml(post.password);
+    var sql = "SELECT * FROM login WHERE username='"+username+"'";
+    con.query(sql, function (err, result, fields) {
+        if( result.length > 0 && result[0].password === password ) {
+            console.log("Auth set");
+            req.session.username = post.user;
+            res.send({"result" : "Found"});
+        }else {
+            res.send({"result": "NotFound"});
+        }
+    });
 });
 app.get('/admin'  ,  function (req , res) {
-    console.log(req.session.user);
-    if(!req.session.user) {
-        console.log("Admin");
+    if(!req.session.username) {
         res.redirect('/login');
     }else {
         res.sendFile(__dirname + '/www/admin.html');
     }
 });
-app.get('/speaker' , function (req , res) { 
+app.get('/speakers' , function (req , res) {
 	res.sendFile(__dirname + '/www/speaker.html');
 });
-app.post('/speaker' , function (req , res) { 
+app.post('/speakers' , function (req , res) {
     var sql = "SELECT * FROM speaker ";
     con.query(sql, function (err, result, fields) {
-        var jsonString = JSON.stringify(result);
-        var jsonData = JSON.parse(jsonString);
-        res.send(jsonData);
+        res.send(result);
     });
 });
-app.post("/speakerinsert", function(req, res) {
+app.post("/speaker-insert", function(req, res) {
     upload(req ,res, function(err) {
         if (err) {
             return res.end("Something went wrong!");
+        }else {
+            const post = req.body;
+            const name  = sanitizeHtml(post.speakername);
+            const topic = sanitizeHtml(post.topic);
+            const description = sanitizeHtml(post.description);
+            var sql = "INSERT INTO speaker(name, topic, description, pic_url) " +
+                        "values('"+name+"','"+topic+"','"+description+"','/images/"+name+".jpg')";
+            con.query(sql, function (err, result, fields) {
+                res.redirect('/admin');
+            });
         }
-        insertSpeaker(req , res);
     });
 });
 app.get('/logout', function (req, res) {
@@ -83,7 +100,9 @@ app.get('/logout', function (req, res) {
     res.redirect("/login");
 });
 app.post('/getuser' , function (req , res) {
-    res.send({"username" : req.session.user});
+    res.send({
+        "username" : req.session.username
+    });
 });
 app.get('/blog' , function (req , res) { 
 	res.sendFile(__dirname + '/www/blog.html');
@@ -91,60 +110,39 @@ app.get('/blog' , function (req , res) {
 app.post('/blog' , function (req , res) { 
     var sql = "SELECT * FROM videos";
     con.query(sql, function (err, result, fields) {
-        var jsonString = JSON.stringify(result);
-        var jsonData = JSON.parse(jsonString);
-        res.send(jsonData);
+        res.send(result);
     });
 });
-app.post("/videoinsert", function(req, res) {
-    insertVideo(req , res);
-});
-function login(req,res){
-    var post = req.body;
-    var username  = post.user;
-    var password = post.password;
-    username = sanitizeHtml(username);
-    password = sanitizeHtml(password);
-    var sql = "SELECT * FROM login WHERE username='"+username+"'";
-    con.query(sql, function (err, result, fields) {
-        var jsonString = JSON.stringify(result);
-        var jsonData = JSON.parse(jsonString);
-        if( jsonData.length > 0 && jsonData[0].password === password ) {
-            console.log(req.session.user + "Auth set");
-            req.session.user = post.user;
-            res.send({"result" : "Found"});
-        }else {
-            res.send({"result": "NotFound"});
-        }
-    });
-}
-function insertSpeaker(req , res ) {
-    var post = req.body;
-    var name  = post.speakername;
-    var topic = post.topic;
-    var description = post.description;
-    name = sanitizeHtml(name);
-    topic = sanitizeHtml(topic);
-    description = sanitizeHtml(description);
-    var sql = "INSERT INTO speaker(name, topic, description, pic_url) values('"+name+"','"+topic+"','"+description+"','/images/"+name+".jpg')";
+app.post("/video-insert", function(req, res) {
+    const post = req.body;
+    const title  = sanitizeHtml(post.title);
+    const description = sanitizeHtml(post.description);
+    const url = sanitizeHtml(post.url);
+    var sql = "INSERT INTO videos(title, description, video_url) " +
+                "values('"+title+"','"+description+"','"+url+"')";
     con.query(sql, function (err, result, fields) {
         res.redirect('/admin');
     });
-}
-function insertVideo(req , res ) {
-    var post = req.body;
-    var title  = post.title;
-    var description = post.description;
-    var url = post.url;
-    title = sanitizeHtml(title);
-    description = sanitizeHtml(description);
-    var sql = "INSERT INTO videos(title, description, video_url) values('"+title+"','"+description+"','"+url+"')";
+});
+app.post("/delete-speaker", function(req, res) {
+    const post = req.body;
+    const speakerName  = sanitizeHtml(post.speakerName);
+    var sql = "Delete from speaker where name = '" + speakerName +"'" ;
     con.query(sql, function (err, result, fields) {
         res.redirect('/admin');
     });
-}
+});
+app.post("/delete-video", function(req, res) {
+    const post = req.body;
+    const videoTitle  = sanitizeHtml(post.videoTitle);
+    var sql = "Delete from videos where title = '" + videoTitle +"'" ;
+    con.query(sql, function (err, result, fields) {
+        res.redirect('/admin');
+    });
+});
 // Starting Server
 const port = process.env.PORT || 3000;
 server.listen(port, function(){
     console.log('listening on *:3000');
 });
+
